de5db4dc681476fb8afeb767daa810de4dde5e4d
itstudentua <itstudentua@gmail.com>
2022-11-08 22:51:02 +0200

    work with states

--- src/App.js
+++ src/App.js
@@ -4,25 +4,39 @@ import Image from "./components/Image";
 import logo from "./img/react-log.svg"
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            helpText: "Help text",
+            userData: "",
+        }
+
+        this.inputClick = this.inputClick.bind(this) // эта строка нужна, чтобы изменять состояния в собственных методах
+    }
     render() {
         return(
             <div className="main-section">
                 <Header title="Шапка сайта"/>
-                <h1>Hello React!</h1>
+                <h1>{this.state.helpText}</h1>
+                <h2>{this.state.userData}</h2>
                 <input
-                    placeholder={this.helpText}
+                    placeholder={this.state.helpText}
+                    onChange={event => this.setState({userData: event.target.value})}
                     onClick={this.inputClick}
                     onMouseEnter={this.mouseOver}
                 />
-                <span>{this.helpText === "hello"? "YES" : "NO"}</span>
+                <span>{this.state.helpText === "hello"? "YES" : "NO"}</span>
                 <Image image={logo}/>
-                <img src={logo}/>
+                <img src={logo} alt={this.state.helpText}/>
 
             </div>
         );
     }
-    helpText = "Help text";
-    inputClick() { alert('clicked') }
+    inputClick() {
+        alert('clicked');
+        this.setState({ helpText: "Changed" })
+    }
     mouseOver () { console.log('mouse over') }
 }
 
