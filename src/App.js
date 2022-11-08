6e595fd4f428edba234fab43eb5cb1111d38d62f
itstudentua <itstudentua@gmail.com>
2022-11-08 21:45:06 +0200

    linking css and img files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Image from "./components/Image";
+import logo from "./img/react-log.svg"
 
 class App extends React.Component {
     render() {
@@ -13,6 +14,10 @@ class App extends React.Component {
                     onClick={this.inputClick}
                     onMouseEnter={this.mouseOver}
                 />
+                <span>{this.helpText === "hello"? "YES" : "NO"}</span>
+                <Image image={logo}/>
+                <img src={logo}/>
+
             </div>
         );
     }
